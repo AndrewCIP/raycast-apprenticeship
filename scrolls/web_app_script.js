0bcf3675131e5bcb276e7827fc5aa3598587adb8
AndrewCIP <a_lewis18@u.pacific.edu>
2026-01-14 10:41:23 -0800

    Updated web_app_script.js

--- scrolls/web_app_script.js
+++ scrolls/web_app_script.js
@@ -24,6 +24,24 @@ async function init() {
   // Create a gpu command encoder
   // The encoder allows us to craft different rendering spells
   const encoder = device.createCommandEncoder();
+
+  // Vertex shader code
+  var vertCode = `
+  @vertex // this compute the scene coordinate of each input vertex
+  fn vertexMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
+  return vec4f(pos, 0, 1); // (pos, Z, W) = (X, Y, Z, W)
+  }
+    `;
+
+  var fragCode = `
+  @fragment // this compute the color of each pixel
+  fn fragmentMain(@location(0) color: vec4f) -> @location(0) vec4f {
+  return color; // (R, G, B, A)
+  }
+  `;
+
+  pass.draw(vertices.length / 6);        // how many vertices to draw
+
   // Use the encoder to begin render pass
   const pass = encoder.beginRenderPass({
     colorAttachments: [{
