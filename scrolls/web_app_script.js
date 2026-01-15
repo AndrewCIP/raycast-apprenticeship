8e3bb50dbc4108450db1b530a2acf44d028f89c4
AndrewCIP <a_lewis18@u.pacific.edu>
2026-01-15 02:47:52 -0800

    Abstracted Existing Code To New Classes

--- scrolls/web_app_script.js
+++ scrolls/web_app_script.js
@@ -1,136 +1,31 @@
-async function init() {
-  // Create a canvas tag, where we render on the browser
-  const canvasTag = document.createElement('canvas');
-  canvasTag.id = "renderCanvas"; // Important! This tells which CSS style to use
-   // Modify the canvas size
- const devicePixelRatio = window.devicePixelRatio || 1;
- const width = window.innerWidth * devicePixelRatio;
- const height = window.innerHeight * devicePixelRatio;
- canvasTag.width = width;
- canvasTag.height = height; 
- // Modify the canvas using CSS
- canvasTag.style.width = `${window.innerWidth}px`;
- canvasTag.style.height = `${window.innerHeight}px`;
-  document.body.appendChild(canvasTag);
-  // Check if the browser supports WebGPU
-  if (!navigator.gpu) {
-    throw Error("WebGPU is not supported in this browser.");
-  }
-  // Get an GPU adapter - use the API to get a GPU adapter
-  const adapter = await navigator.gpu.requestAdapter();
-  if (!adapter) {
-    throw Error("Couldn't request WebGPU adapter.");
-  }
-  // Get a GPU device - use the adapter to request a GPU
-  const device = await adapter.requestDevice();
-  // Get canvas context using webgpu
-  const context = canvasTag.getContext("webgpu");
-  const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
-  context.configure({
-    device: device,
-    format: canvasFormat,
-  });
-  // Create a gpu command encoder
-  // The encoder allows us to craft different rendering spells
-  const encoder = device.createCommandEncoder();
+import Renderer from '/lib/Viz/2DRenderer.js'
+ import Triangle1 from '/lib/Scene/Triangle1.js'
+ import Triangle2 from '/lib/Scene/Triangle2.js'
 
- // Create a triangle geometry in CPU
-  var vertices = new Float32Array([
- // x, y
-   0, 0.5,
-   -0.5, 0,
-   0.5,  0,
-   0, 0.5, // line strip draw a loop, so set the last vertex the same as the first
- ]);
+ async function init() {
+   // Create a canvas tag
+   const canvasTag = document.createElement('canvas');
+   canvasTag.id = "renderCanvas";
+   document.body.appendChild(canvasTag);
 
- // Create vertex buffer to store the vertices in GPU
- var vertexBuffer = device.createBuffer({
-   label: "Vertices",
-   size: vertices.byteLength,
-   usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
- });
- // Copy from CPU to GPU
- device.queue.writeBuffer(vertexBuffer, 0, vertices);
+   // Create a simple renderer
+   const renderer = new Renderer(canvasTag);
+   await renderer.init();
 
- // Define vertex buffer layout - how the shader should read the buffer
- var vertexBufferLayout = {
-   arrayStride: 2 * Float32Array.BYTES_PER_ELEMENT,
-   attributes: [{ 
-     // position 0 has two floats
-     shaderLocation: 0,   // position in the vertex shader
-     format: "float32x2", // two coordinates
-     offset: 0,           // no offset in the vertex buffer
-   }],
- };
+   // Append objects
+   await renderer.appendSceneObject(new Triangle1(renderer._device, renderer._canvasFormat));
+   await renderer.appendSceneObject(new Triangle2(renderer._device, renderer._canvasFormat));
 
- // Vertex shader code
- var vertCode = `
- @vertex // this compute the scene coordinate of each input vertex
- fn vertexMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
-   return vec4f(pos, 0, 1); // (pos, Z, W) = (X, Y, Z, W)
+   // Render
+   renderer.render();
+   return renderer;
  }
- `;
- // Fragment shader code
- var fragCode = `
- @fragment // this compute the color of each pixel
- fn fragmentMain() -> @location(0) vec4f {
-   return vec4f(238.f/255, 118.f/255, 35.f/255, 1); // (R, G, B, A)
- }
- `;
 
- var shaderModule = device.createShaderModule({
-   label: "Shader",
-   code: vertCode + '\n' + fragCode,
+ init().then( ret => {
+   console.log(ret);
+ }).catch( error => {
+   const pTag = document.createElement('p');
+   pTag.innerHTML = navigator.userAgent + "</br>" + error.message;
+   document.body.appendChild(pTag);
+   document.getElementById("renderCanvas").remove();
  });
-
- var renderPipeline = device.createRenderPipeline({
-   label: "Render Pipeline",
-   layout: "auto",
-   vertex: {
-     module: shaderModule,          // the shader code
-     entryPoint: "vertexMain",      // the shader function
-     buffers: [vertexBufferLayout]  // the binded buffer layout
-   },
-   fragment: {
-     module: shaderModule,          // the shader code
-     entryPoint: "fragmentMain",    // the shader function
-     targets: [{
-       format: canvasFormat         // the target canvas format
-     }]
-   },
-   primitive: {                     // instead of drawing triangles
-     topology: 'line-strip'         // draw line strip
-   }
- });
-
-  // Use the encoder to begin render pass
-  const pass = encoder.beginRenderPass({
-    colorAttachments: [{
-      view: context.getCurrentTexture().createView(),
-      clearValue: { r: 4/225, g: 106/255, b: 29/255, a: 1 }, // Blue
-      loadOp: "clear",
-      storeOp: "store",
-    }]
-  });
-   // add more render pass to draw the plane
- pass.setPipeline(renderPipeline);      // which render pipeline to use
- pass.setVertexBuffer(0, vertexBuffer); // which vertex buffer is used at location 0
- pass.draw(vertices.length / 2);        // how many vertices to draw
-  pass.end(); // end the pass
-  // Create the command buffer using the encoder
-  const commandBuffer = encoder.finish();
-  // Submit it to the device to render
-  device.queue.submit([commandBuffer]);
-  return context;
-}
-
-init().then( ret => {
-  console.log(ret);
-}).catch( error => {
-  // Error Handling - add a p tag to display the error message
-  const pTag = document.createElement('p');
-  pTag.innerHTML = navigator.userAgent + "</br>" + error.message;
-  document.body.appendChild(pTag);
-  // also remove the created canvas tag
-  document.getElementById("renderCanvas").remove();
-});
