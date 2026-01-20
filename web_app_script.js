4869b178c11b5305cb14800b306f5bd401d78a21
Andrew Lewis <145711985+AndrewCIP@users.noreply.github.com>
2026-01-19 19:28:28 -0800

    Update web_app_script.js

--- web_app_script.js
+++ web_app_script.js
@@ -20,7 +20,9 @@ import Renderer from '/lib/Viz/2DRenderer.js'
    await renderer.appendSceneObject(tri2);
 
    tri1.setPosition(2, 0.0);
+   tri1.updateTRansform();
    tri2.setPosition(-2, 0.0);
+   tri2.updateTransform();
 
    // Render
    renderer.render();
