fa3eb8987362a8b77c35f7cf956bae19956fdf5a
AndrewCIP <a_lewis18@u.pacific.edu>
2026-01-20 03:21:26 -0800

    Color Applied To Static Scene

--- web_app_script.js
+++ web_app_script.js
@@ -23,7 +23,7 @@ import Renderer from '/lib/Viz/2DRenderer.js'
    // Ground
    await renderer.appendSceneObject(new Square2(renderer._device, renderer._canvasFormat, 0, -0.775, 0.5, 0.1));
    await renderer.appendSceneObject(new Square2(renderer._device, renderer._canvasFormat, 0, -0.85, 0.3, 0.05));
-   await renderer.appendSceneObject(new Triangle2(renderer._device, renderer._canvasFormat, 0, -0.725, 0.35, 0.2));
+   await renderer.appendSceneObject(new Triangle1(renderer._device, renderer._canvasFormat, 0, -0.725, 0.35, 0.2));
    // Tree
    await renderer.appendSceneObject(new Square1(renderer._device, renderer._canvasFormat, 0.0, -0.25, 0.025, 0.75));
    // Leaves
