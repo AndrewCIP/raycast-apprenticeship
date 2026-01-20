f612ae023e87cac9a8e400cf98f80cdb8a583fcb
AndrewCIP <a_lewis18@u.pacific.edu>
2026-01-20 02:24:46 -0800

    Building Picture

--- web_app_script.js
+++ web_app_script.js
@@ -25,7 +25,7 @@ import Renderer from '/lib/Viz/2DRenderer.js'
    await renderer.appendSceneObject(new Square2(renderer._device, renderer._canvasFormat, 0, -0.85, 0.3, 0.05));
    // Trees
    await renderer.appendSceneObject(new Square2(renderer._device, renderer._canvasFormat, 0.3, -0.25, 0.025, 0.75));
-   await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, 0, -0.25, 0.2, 0.25));
+   await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, 0.1, -0.25, 0.2, 0.25));
    await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, 0.3, 0.2, 0.3, 0.3));
    await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, 0.2, 0, 0.2, 0.2));
    // Branches
