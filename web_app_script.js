2493ebb1f73e8d18b365f6c118d26304734eca81
AndrewCIP <a_lewis18@u.pacific.edu>
2026-01-20 03:52:04 -0800

    Building Picture

--- web_app_script.js
+++ web_app_script.js
@@ -45,6 +45,7 @@ import Renderer from '/lib/Viz/2DRenderer.js'
    await renderer.appendSceneObject(new Hexagon1(renderer._device, renderer._canvasFormat, 0.1, 0.25, 0.03, 0.03));
    await renderer.appendSceneObject(new Hexagon1(renderer._device, renderer._canvasFormat, 0.2, 0.15, 0.03, 0.03));
    await renderer.appendSceneObject(new Hexagon1(renderer._device, renderer._canvasFormat, 0.175, -0.35, 0.05, 0.05));
+   await renderer.appendSceneObject(new Hexagon1(renderer._device, renderer._canvasFormat, 0.125, -0.4, 0.05, 0.05));
 
    // Render
    renderer.render();
