4397e1a0f0cfe2ca27d813a0a8cc0ed1ffc42532
AndrewCIP <a_lewis18@u.pacific.edu>
2026-01-20 02:48:17 -0800

    Building Picture

--- web_app_script.js
+++ web_app_script.js
@@ -25,18 +25,18 @@ import Renderer from '/lib/Viz/2DRenderer.js'
    await renderer.appendSceneObject(new Square2(renderer._device, renderer._canvasFormat, 0, -0.85, 0.3, 0.05));
    await renderer.appendSceneObject(new Triangle2(renderer._device, renderer._canvasFormat, 0, -0.725, 0.35, 0.2));
    // Tree
-   await renderer.appendSceneObject(new Square2(renderer._device, renderer._canvasFormat, 0.3, -0.25, 0.025, 0.75));
+   await renderer.appendSceneObject(new Square2(renderer._device, renderer._canvasFormat, 0.0, -0.25, 0.025, 0.75));
    // Leaves
-   await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, 0.1, -0.25, 0.2, 0.25));
-   await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, 0.3, 0.2, 0.3, 0.3));
-   await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, 0.2, 0, 0.2, 0.2));
-   await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, 0.45, -0.15, 0.25, 0.1));
-   await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, 0.475, -0.4, 0.2, 0.15));
-   await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, 0.425, -0.375, 0.175, 0.2));
+   await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, -0.2, -0.25, 0.2, 0.25));
+   await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, 0, 0.2, 0.3, 0.3));
+   await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, -0.1, 0, 0.2, 0.2));
+   await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, 0.15, -0.15, 0.25, 0.1));
+   await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, 0.175, -0.4, 0.2, 0.15));
+   await renderer.appendSceneObject(new Circle2(renderer._device, renderer._canvasFormat, 0.125, -0.375, 0.175, 0.2));
    // Branches
-   await renderer.appendSceneObject(new Square2(renderer._device, renderer._canvasFormat, 0.2, -0.25, 0.2, 0.02));
-   await renderer.appendSceneObject(new Square2(renderer._device, renderer._canvasFormat, 0.35, -0.4, 0.1, 0.02));
-   await renderer.appendSceneObject(new Square2(renderer._device, renderer._canvasFormat, 0.325, -0.15, 0.05, 0.02));
+   await renderer.appendSceneObject(new Square2(renderer._device, renderer._canvasFormat, -0.1, -0.25, 0.2, 0.02));
+   await renderer.appendSceneObject(new Square2(renderer._device, renderer._canvasFormat, 0.05, -0.4, 0.1, 0.02));
+   await renderer.appendSceneObject(new Square2(renderer._device, renderer._canvasFormat, 0.025, -0.15, 0.05, 0.02));
 
    // Render
    renderer.render();
