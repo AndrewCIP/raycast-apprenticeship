4156ef64f72e8c00cbcfbe2c63a21af12cbbda62
AndrewCIP <a_lewis18@u.pacific.edu>
2026-01-19 20:50:27 -0800

    Updated Triangle1

--- web_app_script.js
+++ web_app_script.js
@@ -13,8 +13,8 @@ import Renderer from '/lib/Viz/2DRenderer.js'
    await renderer.init();
 
    // Append objects
-   await renderer.appendSceneObject(new Triangle1(renderer._device, renderer._canvasFormat), 0, 0);
-   await renderer.appendSceneObject(new Triangle2(renderer._device, renderer._canvasFormat));
+   await renderer.appendSceneObject(new Triangle1(renderer._device, renderer._canvasFormat, 0, 0));
+   await renderer.appendSceneObject(new Triangle2(renderer._device, renderer._canvasFormat, 0, 0));
 
    // Render
    renderer.render();
