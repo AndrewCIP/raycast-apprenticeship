20b4983bf96169225402e5e67fb58eb44f1feefa
AndrewCIP <a_lewis18@u.pacific.edu>
2026-01-22 13:21:09 -0800

    ImageNosifyFilterObject Fixed

--- web_app_script.js
+++ web_app_script.js
@@ -2,6 +2,7 @@ import Renderer from '/lib/Viz/FilteredRenderer.js'
 import Standard2DFullScreenObject from "/lib/Scene/Standard2DFullScreenObject.js";
  import ImageFilterObject from "/lib/Scene/ImageFilterObject.js";
  import ImageNosifyFilterObject from "/lib/Scene/ImageNosifyFilterObject.js";
+ import Standard2DGAPosedVertexObject from '/lib/Scene/Standard2DGAPosedVertexObject.js';
  import Triangle1 from '/lib/Scene/Triangle1.js'
  import Triangle2 from '/lib/Scene/Triangle2.js'
  import Square1 from '/lib/Scene/Square1.js'
@@ -21,6 +22,19 @@ import Standard2DFullScreenObject from "/lib/Scene/Standard2DFullScreenObject.js
    // Create a simple renderer
    const renderer = new Renderer(canvasTag);
    await renderer.init();
+
+  let applyRotorToRotor = (dr, r) => {
+   // r = cS + s exey
+   // dr = ccS + ss exey
+   // dr r = (c * cc - s * ss)S + (cc * s + ss * s) e01
+   return [dr[0] * r[0] - dr[1] * r[1], dr[0] * r[1] + dr[1] * r[0]];
+ };
+
+  var pose = [1, 0, 0, 0, 1, 1]; // rotor, translator, scales
+  pose = new Float32Array(pose);
+  await renderer.appendSceneObject(new Standard2DGAPosedVertexObject(renderer._device, renderer._canvasFormat, vertices, pose, "/lib/Shaders/ga_pose.wgsl", "triangle-list"));
+  let angle = Math.PI / 100 / 2;
+  let dr = [Math.cos(angle), -Math.sin(angle)]; // a delta rotor
 
    // Append objects
 
@@ -73,6 +87,14 @@ import Standard2DFullScreenObject from "/lib/Scene/Standard2DFullScreenObject.js
    renderer.render();
    return renderer;
  }
+
+ setInterval(() => { 
+   renderer.render();
+   // update pose
+   let newrotor = applyRotorToRotor(dr, [pose[0], pose[1]]);
+   pose[0] = newrotor[0];
+   pose[1] = newrotor[1];
+ }, 100); // call every 100 ms
 
  init().then( ret => {
    console.log(ret);
