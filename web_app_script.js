0e8f2cc5862da9db0a7792e27c62a9f79e26cd7e
AndrewCIP <a_lewis18@u.pacific.edu>
2026-01-22 00:18:51 -0800

    Updated Main Script

--- web_app_script.js
+++ web_app_script.js
@@ -24,8 +24,8 @@ import Standard2DFullScreenObject from "/lib/Scene/Standard2DFullScreenObject.js
    // Append objects
 
    // Add Image Background
-   //await renderer.appendSceneObject(new Standard2DFullScreenObject(renderer._device, renderer._canvasFormat, "/assets/kirby_background.jpg"));
-   await renderer.appendFilterObject(new ImageFilterObject(renderer._device, renderer._canvasFormat, "/lib/Shaders/dummy.wgsl"));
+   await renderer.appendSceneObject(new Standard2DFullScreenObject(renderer._device, renderer._canvasFormat, "/assets/kirby_background.jpg"));
+   //await renderer.appendFilterObject(new ImageFilterObject(renderer._device, renderer._canvasFormat, "/lib/Shaders/dummy.wgsl"));
 
    /*
    // Background Mountains
