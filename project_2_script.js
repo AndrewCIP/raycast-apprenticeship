a8fe795a4f82d464076fb0ee3792c8d54782a7f9
AndrewCIP <a_lewis18@u.pacific.edu>
2026-01-26 01:11:35 -0800

    Testing project_2_script

--- project_2_script.js
+++ project_2_script.js
@@ -163,49 +163,48 @@ async function init() {
   await createOrbitingObject({
     radius: 0.00,
     speed: 0.00,
-    size: 0.05
   });
 
   // Mercury
   await createOrbitingObject({
     radius: 0.10,
     speed: 0.04,
-    size: 0.02
+    size: 0.005
   });
 
   // Perfect circle
   await createOrbitingObject({
     radius: 0.15,
     speed: 0.03,
-    size: 0.04
+    size: 0.008
   });
 
   // Perfect circle
   await createOrbitingObject({
     radius: 0.20,
     speed: 0.025,
-    size: 0.05
+    size: 0.001
   });
 
   // Perfect circle
   await createOrbitingObject({
     radius: 0.25,
     speed: 0.02,
-    size: 0.035
+    size: 0.007
   });
 
   // Perfect circle
   await createOrbitingObject({
     radius: 0.35,
     speed: 0.012,
-    size: 0.12
+    size: 0.024
   });
 
   // Smaller counter-rotating circle
   await createOrbitingObject({
     radius: 0.45,
     speed: 0.01,
-    size: 0.10
+    size: 0.02
   });
 
   // Elliptical orbit
@@ -220,14 +219,14 @@ async function init() {
   await createOrbitingObject({
     ellipse: { a: 0.6, b: 0.3 },
     speed: 0.007,
-    size: 0.06
+    size: 0.012
   });
 
   // Elliptical orbit
   await createOrbitingObject({
     ellipse: { a: 0.8, b: 0.4 },
     speed: 0.005,
-    size: 0.06
+    size: 0.012
   });
 
   /* --------------------------------------------------
