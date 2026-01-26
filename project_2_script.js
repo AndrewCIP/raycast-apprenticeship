793095798d4f12fcdb8e1bc814537225ff6777c5
AndrewCIP <a_lewis18@u.pacific.edu>
2026-01-26 03:40:04 -0800

    Project 2 Sealed

--- project_2_script.js
+++ project_2_script.js
@@ -105,7 +105,8 @@ async function init() {
     center = [0, 0],
     ellipse = null,
     size = 0.02,
-    parent = null  // <--- new parent
+    parent = null,  // <--- new parent
+    shader = '/lib/Shaders/projective_geometric_algebra.wgsl'
   }) {
 
   const planet = new PlanetObject(renderer._device, renderer._canvasFormat, size);
@@ -128,7 +129,7 @@ async function init() {
         renderer._canvasFormat,
         planet._vertices,
         pose,
-        "/lib/Shaders/projective_geometric_algebra.wgsl",
+        shader,
         "triangle-list"
       )
     );
@@ -162,6 +163,7 @@ async function init() {
   await createOrbitingObject({
     radius: 0.00,
     speed: 0.00,
+    shader: '/lib/Shaders/pot1.wgsl'
   });
 
   // Mercury
