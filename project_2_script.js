ad001366e71cf800a5342ab83af5804e16eba05a
AndrewCIP <a_lewis18@u.pacific.edu>
2026-01-25 23:09:35 -0800

    Testing project_2_script

--- project_2_script.js
+++ project_2_script.js
@@ -39,7 +39,7 @@ async function init() {
   const renderer = new Renderer(canvasTag);
   await renderer.init();
 
-  const planet = new PlanetObject(renderer._device, renderer._canvasFormat, 0.15, 48);
+  const planet = new PlanetObject(renderer._device, renderer._canvasFormat, 0.015, 48);
 
   // initial motor
   let pose0 = normalizeMotor([1, 0, -0.3, 0]);
