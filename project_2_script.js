de56507824e6e6e233c96919fa338468790c598b
AndrewCIP <a_lewis18@u.pacific.edu>
2026-01-26 01:05:45 -0800

    Testing project_2_script

--- project_2_script.js
+++ project_2_script.js
@@ -206,7 +206,7 @@ async function init() {
   // Elliptical orbit
   await createOrbitingObject({
     radius: 0.025,
-    speed: 0.007,
+    speed: 0.02,
     size: 0.005,
     parent: orbitingObjects[1]
   });
