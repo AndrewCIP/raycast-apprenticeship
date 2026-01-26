69183a108f55c70a3afd5d35b5424911f25b82ff
AndrewCIP <a_lewis18@u.pacific.edu>
2026-01-26 10:46:32 -0800

    Scroll 4 Assignment Part 2 Fixed

--- scroll_4_script.js
+++ scroll_4_script.js
@@ -11,7 +11,7 @@ async function init() {
   const renderer = new Renderer(canvasTag);
   await renderer.init();
   let camera = new Camera();
-  let triangle = new Camera2DVertexObject(renderer._device, renderer._canvasFormat, camera._pose, new Float32Array([0, 0.5, -0.5, 0, 0.5, 0]), "<your new shader file>");
+  let triangle = new Camera2DVertexObject(renderer._device, renderer._canvasFormat, camera._pose, new Float32Array([0, 0.5, -0.5, 0, 0.5, 0]), "reverse_camera_pga.wgsl");
   await renderer.appendSceneObject(triangle);
 
   // run animation at 60 fps
